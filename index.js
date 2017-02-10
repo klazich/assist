7dfdb4a000517704728a0e714976e55e487add76
Kevin <kevin.lazich@gmail.com>
2017-02-10 00:09:17 -0500

    restructuring?

--- index.js
+++ index.js
@@ -1,45 +1,26 @@
-#!/usr/bin/env node
 'use strict'
 
-const requireDir        = require('require-dir')
 const { join, resolve } = require('path')
-const shell             = require('shelljs')
-const yargs             = require('yargs')
-const colors            = require('chalk')
+const camelCase = require('camelcase')
+const requireDir = require('require-dir')
+const colors = require('chalk')
+const shell = require('shelljs')
+const fs = require('fs')
+const yargs = require('yargs')
+const _ = require('lodash')
 
 // External dependencies to pass to the commands
-let dep = { join, resolve, console, shell, colors, process }
+let dep = { join, resolve, fs, console, colors, shell, process, yargs, _ }
 
+// Internal dependencies
+const inDepFns = requireDir(join(__dirname, 'lib', 'modules'))
+Object.keys(inDepFns).forEach(name => {
+  dep[camelCase(name)] = inDepFns[name](dep)
+})
+console.log(dep);
 // Load commands from folder and pass dependencies
 const commandsFn = requireDir(join(__dirname, 'lib', 'commands'))
-const commands   = Object.keys(commandsFn).map(i => commandsFn[i](dep))
+const commands = Object.keys(commandsFn).map((i) => commandsFn[i](dep))
 
-// Init CLI commands and options
-commands.forEach(cmd => yargs.command(cmd.command, cmd.desc, cmd.builder, cmd.handler))
-
-// Switch QWD if specified from options
-const cwd = resolve(yargs.argv.cwd || process.cwd())
-process.chdir(cwd);
-
-
-yargs
-  .help()
-  .demand(1)
-  .argv
-  // .option({ cwd: { desc: 'Change the current working directory' } })
-  // .argv
-
-
-// structure of commands:
-//
-// 'use strict'
-//
-// module.exports = function (dep) {
-//   let cmd     = {}
-//   cmd.command = '...'
-//   cmd.desc    = '...'
-//   cmd.builder = {}
-//   cmd.handler = function (argv) {}
-//   return cmd
-// }
-//
+// Export commands and modules separately
+module.exports = { commands, modules: dep }
