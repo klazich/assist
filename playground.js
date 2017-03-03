cd50197e58e64ee39dba2bda342d97fbb6115b5d
Kevin <kevin.lazich@gmail.com>
2017-03-03 15:20:45 -0500

    add nyc, modified create command

--- playground.js
+++
@@ -1,3 +0,0 @@
-const _ = require('lodash')
-
-console.log(new Error('test'))
--- test/modules/log.test.js
+++ test/modules/log.test.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const expect = require('chai').expect
-const assert = require('chai').assert
+// const assert = require('chai').assert
 
 const { modules } = require('./../../index')
-const { _, log, colors, readline, stripAnsi } = modules
+const { _, log, colors, stripAnsi } = modules
 
 describe('modules/log.js', () => {
   describe('log.format()', () => {
@@ -12,25 +12,25 @@ describe('modules/log.js', () => {
 
     it('should optput cli header prompt on first line', () => {
       let formatted = stripAnsi(format('test', 'test message'))
-        .split('\n')[ 0 ]
+        .split('\n')[0]
       expect(formatted).to.equal('        << QI/a')
     })
 
     describe('\'title\' should be trimmed and uppercased', () => {
       let message = 'test message'
       let tests = [
-        { args: [ 'title', message ], expected: 'TITLE' },
-        { args: [ 'a space', message ], expected: 'A SPACE' },
-        { args: [ 'TITLE', message ], expected: 'TITLE' },
-        { args: [ '  title', message ], expected: 'TITLE' },
-        { args: [ 'title  ', message ], expected: 'TITLE' }
+        { args: ['title', message], expected: 'TITLE' },
+        { args: ['a space', message], expected: 'A SPACE' },
+        { args: ['TITLE', message], expected: 'TITLE' },
+        { args: ['  title', message], expected: 'TITLE' },
+        { args: ['title  ', message], expected: 'TITLE' }
       ]
 
       tests.forEach(function (o) {
-        it(`'${_.padEnd(o.args[ 0 ] + '\'', 8)} to '${o.expected}'`, () => {
+        it(`'${_.padEnd(o.args[0] + '\'', 8)} to '${o.expected}'`, () => {
           let formatted = stripAnsi(format(...o.args))
-            .split('\n')[ 0 ]
-            .match(/(.*)>/)[ 1 ]
+            .split('\n')[0]
+            .match(/(.*)>/)[1]
             .trim()
           expect(formatted).to.equal(o.expected)
         })
@@ -60,22 +60,27 @@ describe('modules/log.js', () => {
     describe('should color titles correctly', () => {
       let message = 'test message'
       let tests = [
-        { args: [ 'error', message ], expected: [ 'red', colors.red('ERROR') ] },
-        { args: [ 'debug', message ], expected: [ 'yellow', colors.yellow('DEBUG') ] },
-        { args: [ 'log', message ], expected: [ 'cyan', colors.cyan('LOG') ] },
-        { args: [ 'default', message ], expected: [ 'gray', colors.gray('DEFAULT') ] }
+        { args: ['error', message], expected: ['red', colors.red('ERROR')] },
+        { args: ['debug', message], expected: ['yellow', colors.yellow('DEBUG')] },
+        { args: ['log', message], expected: ['cyan', colors.cyan('LOG')] },
+        { args: ['default', message], expected: ['gray', colors.gray('DEFAULT')] }
       ]
 
       tests.forEach(function (o) {
-        it(`'${_.padEnd(o.args[ 0 ] + '\'', 8)} to '${o.expected[1]}' ${o.expected[0]}`, () => {
+        it(`'${_.padEnd(o.args[0] + '\'', 8)} to '${o.expected[1]}' ${o.expected[0]}`, () => {
           let formatted = format(...o.args)
-            .split('\n')[ 0 ]
-            .match(/(.*)\u001b\[90m>/)[ 1 ]
+            .split('\n')[0]
+            .match(/(.*)\u001b\[90m>/)[1]
             .replace(/\s/g, '')
             .trim()
           expect(formatted).to.equal(o.expected[1])
         })
       })
+    })
+  })
+  describe('log.ger()', () => {
+    it('should log string returned from log.format()', () => {
+      log.ger(null, 'test message')
     })
   })
 })
