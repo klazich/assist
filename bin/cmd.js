84c5cf5848fb7a456a7ac0663bda37afe3f94593
Kevin <kevin.lazich@gmail.com>
2017-03-08 18:52:36 -0500

    create.js rework; log.js try/catch addition #this should be in master, was working in wrong branch

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,7 +4,7 @@
 const { join, resolve } = require('path')
 const yargs = require('yargs')
 const { homepage, version } = require(join(__dirname, '../package.json'))
-const { commands } = require('../index.js')
+const { commands, modules: { log } } = require('../index.js')
 
 // Switch CWD if specified from options
 const cwd = resolve(yargs.argv.cwd || process.cwd())
@@ -12,16 +12,21 @@ process.chdir(cwd)
 
 // Init CLI commands and options
 commands.forEach(cmd => yargs.command(cmd.command, cmd.desc, cmd.builder, cmd.handler))
+
 yargs
   .help()
   .options({ cwd: { desc: 'Change the current working directory' } })
   .demand(1)
   .epilog(
-    (homepage
-      ? `| Documentation: ${homepage}\n`
-      : '') +
-    (version
-      ? `| version: ${version}`
-      : ''))
+  (homepage
+    ? `| Documentation: ${homepage}\n`
+    : '') +
+  (version
+    ? `| version: ${version}`
+    : ''))
+  // .fail(function (msg, err) {
+  //   log.ger('error', msg)
+  //   log.ger('error', err)
+  //   process.exit(1)
+  // })
   .argv
-
